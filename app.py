a7c971d465deba1775ecc9ebf2425b2d7011cbdb
hweelin-yeo <hy384@cornell.edu>
2020-03-24 19:15:50 -0400

    correcting mongo

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 import os.path
 from datetime import datetime
 
+from urllib.parse import urlparse
+from pymongo import MongoClient
 # Select which library to use for handling HTTP request.  If running on Google App Engine, use `GAE`.
 # Otherwise, use `Requests` which is based on the `requests` module.
 from requestors import Requests as R
@@ -17,20 +19,26 @@ SYMBOLS = ['BTC.X', 'BSV.X', 'BNB.X', 'BCH.X',
 SYMBOLS_FINAL = ['EOS.X', 'DOGE.X', 'BTC.X',
                'ETH.X', 'LTC.X']
 
+# ---------------------------------------------------------------------
+# Mongo setup
+# https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
+# ---------------------------------------------------------------------
+
+MONGO_URL = os.environ.get('MONGODB_URI')
+if not MONGO_URL:
+  MONGO_URL = "mongodb://heroku_kvptfcm8:vbekldoic9poi92kkp810rvk7@ds141185.mlab.com:41185/heroku_kvptfcm8"
+
+db_name = urlparse(MONGO_URL).path[1:]
+client = MongoClient(MONGO_URL)
+db = client[db_name]
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "App is running."
-
-# ---------------------------------------------------------------------
-# Mongo setup
-# ---------------------------------------------------------------------
-
-mongo_url = "mongodb://heroku_kvptfcm8:vbekldoic9poi92kkp810rvk7@ds141185.mlab.com:41185/heroku_kvptfcm8"
-db_name = urlparse.urlparse(mongo_url).path[1:]
-client = MongoClient(mongo_url)
-self.db = client[db_name]
+def getRuns():
+  res = db.runs.count()
+  print(res)
+  return res
 
 # ---------------------------------------------------------------------
 # Cronjob setup
@@ -58,9 +66,9 @@ def without_token_get_stock_stream(symbol, output_filename, params={}):
 #  f.write(json.dumps(result))
 #  f.close()
 
-  posts = db.posts
-  post_result = posts.insert_one(result)
-  print('One post: {0}'.format(post_result.inserted_id))
+  runs = db.runs
+  run_result = runs.insert_one(result)
+  print('One run: {0}'.format(run_result.inserted_id))
   return result
 
 def get_stock_stream(symbol, output_filename, params={}):
