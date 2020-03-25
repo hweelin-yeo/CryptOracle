f1ae77478673f60e7fd81bc1a261dcae602c8291
hweelin-yeo <hy384@cornell.edu>
2020-03-25 14:37:08 -0400

    back to hourly

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from app import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "interval", minutes=2)
+scheduler.add_job(cronjob, "interval", hours=2)
 
 scheduler.start()
