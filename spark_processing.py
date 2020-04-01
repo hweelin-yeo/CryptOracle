78a14d2583094b7bdfa691f1a4e1b1fcbbcb95e2
hweelin-yeo <hy384@cornell.edu>
2020-04-01 03:37:06 -0400

    change from jsonRDD to normal RDD

--- spark_processing.py
+++ spark_processing.py
@@ -1,13 +1,14 @@
 # Imports
 import sys
+from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, array
 import pandas as pd
 
-from pyspark.sql.types import StructType, StructField, StringType
-
 import pymongo_spark
 pymongo_spark.activate()
+
+from pyspark.sql.types import StructType, StructField, StringType
 
 MONGODB_URI = "mongodb://heroku_kvptfcm8:vbekldoic9poi92kkp810rvk7@ds141185.mlab.com:41185/heroku_kvptfcm8"
 
@@ -19,20 +20,25 @@ schema = StructType([StructField("timestamp", StringType()),
                      StructField("body", StringType())])
 
 def summarize(doc):
-    return {"timestamp": str(doc["created_at"]), 
-            "symbol": str(doc["symbols"]["symbol"]), 
-            "sentiment": str(doc["entities"]["sentiment"]),
-            "body": str(doc["body"])}
+	return (doc["created_at"], 
+            doc["symbols"]["symbol"], 
+            doc["entities"]["sentiment"],
+            doc["body"])
+    # return {"timestamp": str(doc["created_at"]), 
+    #         "symbol": str(doc["symbols"]["symbol"]), 
+    #         "sentiment": str(doc["entities"]["sentiment"]),
+    #         "body": str(doc["body"])}
 
 if __name__ == '__main__':
 
     # Start spark session
-    spark = SparkSession.builder.getOrCreate()
+    # sparkConf = SparkConf().set("spark.jars.packages","org.mongodb.spark:mongo-spark-connector_2.11:2.2.0")
+    sc = SparkSession.builder.config('spark.jars.packages','org.mongodb.spark:mongo-spark-connector_2.11:2.2.0' ).getOrCreate()
+    # sc = SparkContext(conf = sparkConf)
     json_rdd = sc.mongoRDD(MONGODB_URI)
 
-    projected_rdd = data_rdd.map(summarize)
-	train_df = sqlcontext.jsonRDD(projected_rdd, schema)
-	train_df.first()
+    summarize_rdd = data_rdd.map(summarize) # train_df = sqlcontext.jsonRDD(projected_rdd, schema)
+    summarize_rdd.first()
 
     # Stop spark session 
     spark.stop()
